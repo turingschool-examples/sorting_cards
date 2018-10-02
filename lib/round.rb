5d6caf5e043b18ab8290866a7d505c7b8cc17b35
Ben Lee <bendelonlee@gmail.com>
2018-10-02 12:09:23 -0600

    added to round

--- lib/round.rb
+++ lib/round.rb
@@ -4,14 +4,35 @@ class Round
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @current_i = 0
   end
 
   def current_card
-    @deck.cards[0]
+    @deck.cards[@current_i]
   end
 
   def record_guess(hash)
-    @guesses << Guess.new(hash[:value], hash[:suit])
+    new_guess = Guess.new("#{hash[value]} of #{hash[suit]}", )
+    @guesses << new_guess
+    @current_i.increment_loop(3)
+    new_guess
+  end
+
+
+  def number_correct
+    @guesses.count {|g| g.correct?}
+  end
+
+end
+
+class Integer
+  
+  def increment_loop(max, amount = 1, min = 0)
+    if self < max
+      self.public_send("+", amount)
+    else
+      self.public_send("-", self)
+    end
   end
 
 end
