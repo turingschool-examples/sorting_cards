a30b6db1c3596bf2c05f3030867f27f07b9a7a99
Tony Robertson <tonyr729@gmail.com>
2018-10-02 08:47:36 -0600

    Add sanitation to guess response in Guess

--- lib/guess.rb
+++ lib/guess.rb
@@ -4,13 +4,19 @@ class Guess
   def initialize(response, card)
     @response = response
     @card = card
+    @approved_values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "ace", "king", "queen", "jack", "spades", "hearts", "diamonds", "clubs"]
   end
 
   def correct?
-    response_list = @response.split(" ")
+    
+    response_list = @response.split(" ").select do | word |
+      sanitized_word = word.gsub!(/[^0-9A-Za-z]/, '') || word
+      
+      @approved_values.include?(sanitized_word.downcase)
+    end
   
     response_list[0] == card.value &&
-    response_list[2] == card.suit
+    response_list[1] == card.suit
   end
 
   def feedback
