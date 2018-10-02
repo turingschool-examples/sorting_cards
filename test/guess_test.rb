756ff26f6d14e849df96fee43965e5015f421f6c
Tony Robertson <tonyr729@gmail.com>
2018-10-01 20:28:43 -0600

    Add tests to Guess test suite

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -11,17 +11,34 @@ class GuessTest < Minitest::Test
   end
 
   def test_it_exists
-    skip
     assert_instance_of Guess, @guess
   end
 
   def test_it_has_a_instantiated_card
-    skip
-    assert_equal card, @guess.card
+    assert_equal @card, @guess.card
   end
   
   def test_it_has_a_response
-    skip
     assert_equal "Queen of Hearts", @guess.response
+  end
+
+  def test_it_returns_true_if_guess_matches
+    assert_equal true, @guess.correct?
+  end
+  
+  def test_it_returns_false_if_guess_matches
+    guess2 = Guess.new("Ace of Spades", @card)
+
+    assert_equal false, guess2.correct?
+  end
+  
+  def test_it_returns_expected_feedback_when_correct
+    assert_equal "Correct!", @guess.feedback
+  end
+
+  def test_it_returns_expected_feedback_when_incorrect
+    guess2 = Guess.new("Ace of Spades", @card)
+
+    assert_equal "Incorrect.", guess2.feedback
   end
 end
