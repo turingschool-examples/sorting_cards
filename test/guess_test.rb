f628283320b7171219142a993537d0f8cf663972
Ben Lee <bendelonlee@gmail.com>
2018-10-02 08:12:37 -0600

    Adding TestIncorrect class

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -36,20 +36,9 @@ class TestCorrect < GuessTest
 
 end
 
-# #=> #<Card:0x00007f9984004cc0 @suit="Hearts", @value="10">
-#
-# pry(main)> guess = Guess.new("10 of Hearts", card)
-# #=> #<Guess:0x00007f99842f0998 @card=#<Card:0x00007f9984004cc0 @suit="Hearts", @value="10">, @guess="10 of Hearts">
-#
-# pry(main)> guess.card
-# #=> #<Card:0x00007f9984004cc0 @suit="Hearts", @value="10">
-#
-# pry(main)> guess.response
-# #=> "10 of Hearts"
-#
-# pry(main)> guess.correct?
-# #=> true
-#
-# pry(main)> guess.feedback
-# #=> "Correct!"
-# end
+class TestIncorrect < GuessTest
+
+  def setup
+    @card = Card.new("Queen", "Clubs")
+  end
+end
