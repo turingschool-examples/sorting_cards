90e257d7f5edf74bce4acd4a109a66279201258f
Ben Lee <bendelonlee@gmail.com>
2018-10-02 12:09:44 -0600

    added round test

--- lib/round.rb
+++ lib/round.rb
@@ -11,22 +11,25 @@ class Round
     @deck.cards[@current_i]
   end
 
-  def record_guess(hash)
-    new_guess = Guess.new("#{hash[value]} of #{hash[suit]}", )
+  def record_guess(args)
+    new_guess = Guess.new("#{args[value]} of #{args[suit]}", current_card)
     @guesses << new_guess
     @current_i.increment_loop(3)
     new_guess
   end
 
-
   def number_correct
     @guesses.count {|g| g.correct?}
+  end
+
+  def percent_correct
+    number_correct * 100.0 / @guesses.size
   end
 
 end
 
 class Integer
-  
+
   def increment_loop(max, amount = 1, min = 0)
     if self < max
       self.public_send("+", amount)
---
+++ test/round_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/guess'
+require_relative '../lib/card'
+require_relative '../lib/deck'
+require_relative '../lib/round'
+
+class TestRound < Minitest::Test
+  def setup
+    @deck = [Card.new("3","Hearts"), Card.new("4", "Clubs")]
+    @round = Round.new(@deck)
+  end
+
+  def test_it_has_guesses_array
+
+    assert_equal @round.guesses, []
+
+  end
+
+  def test_current_card_gives_first_card_initially
+
+    assert_equal @round.current_card.suit, "Hearts"
+    assert_equal @round.current_card.value, "3"
+
+  end
+
+  def test_record_guess
+
+    new_guess = round.record_guess({value: "3", suit: "Hearts"})
+    assert_instance_of Guess, new_guess
+    assert new_guess.correct?
+    assert_includes round.guesses, new_guess
+    assert_equal 1, @round.number_correct
+    new_current_card = @round.current_card
+    assert_equal "4", new_current_card.value
+    assert_equal "Clubs", new_current_card.suit
+    round.record_guess({value: "Jack", suit: "Diamonds"})
+    assert_equal 2, round.guesses.count
+    assert_equal "Incorrect.", round.guesses.last.feedback
+    assert_equal 1, round.number_correct
+    assert_equal 50.0, round.percent_correct
+
+  end
+
+
+
+
+end
