ff3e40a50d2bcc0f07286fcffd6046b8895109fe
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-03 15:56:35 -0600

    Working on Iteration 1. Have initialized deck and about to created the .cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,26 @@
+
+
 class Deck
-  
+  attr_accessor :deck
+
+  def initialize(deck)
+  end
+
+  # def cards
+  #   # return deck (in an array)
+  #   @deck
+  # end
+
 end
+
+=begin
+
+require './lib/card'
+require './lib/deck'
+card_1 = Card.new("3","Hearts")
+card_2 = Card.new("4", "Clubs")
+card_3 = Card.new("5", "Diamonds")
+cards = [card_1, card_2, card_3]
+deck = Deck.new(cards)
+
+=end
