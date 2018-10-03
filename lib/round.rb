34a5c56fd9b6103e54f7cacc55cfe1a04d2dfb64
Rene Casco <rene.casco@icloud.com>
2018-10-02 23:58:58 -0600

    Iteration 2 WIP

--- lib/round.rb
+++ lib/round.rb
@@ -1,1 +1,25 @@
 require './lib/deck'
+require './lib/guess'
+
+class Round
+  attr_reader :deck,
+              :guesses
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+  end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  def record_guess(guess)
+    response = "#{guess[:value]} of #{guess[:suit]}"
+    @guesses << Guess.new(response, current_card)
+    @deck.cards.shift
+    return @guesses.last
+  end
+
+
+end
--- test/round_test.rb
+++ test/round_test.rb
@@ -5,8 +5,46 @@ require './lib/round'
 class RoundTest < Minitest::Test
 
   def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Round, round
+  end
 
-    
+  def test_it_has_arguments
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
+
+  def test_that_guesses_is_initialized_with_empty_array
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
+  end
+
+  def test_that_current_card_returns_first_item_of_array
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
+
+  def test_that_it_records_first_guess
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    new_guess = round.record_guess({value: "3", suit: "Hearts"})
+    assert_instance_of Guess, new_guess
+    assert_equal true, new_guess.correct?
+    assert_equal [new_guess], round.guesses
   end
 
 end
