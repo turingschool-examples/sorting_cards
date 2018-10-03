06c3d8341786754a1d51578b4a6b0bb8551e707d
Rene Casco <rene.casco@icloud.com>
2018-10-02 21:56:07 -0600

    Deck class completed

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,4 +12,16 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
     assert_instance_of Deck, deck
   end
+
+  def test_it_has_attributes
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal [card_1, card_2, card_3], deck.cards
+    assert_equal 3, deck.count
+  end
+
+
 end
