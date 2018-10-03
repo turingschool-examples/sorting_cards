294e4400b7370b6fb76025f226832d99a7e0c88d
Ben Lee <bendelonlee@gmail.com>
2018-10-03 17:08:38 -0600

    adds a test to deck_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -23,14 +23,13 @@ class CardTest < Minitest::Test
     assert_equal deck.merge_sort, [card_1, card_3, card_4, card_5, card_2]
   end
 
-  def test__merge_sort
-    skip
+  def test__merge_sort_suits
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("4", "Spades")
     card_3 = Card.new("4", "Diamonds")
     card_4 = Card.new("4", "Clubs")
     card_5 = Card.new("Ace", "Diamonds")
-    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    deck = Deck.new([card_4, card_3, card_1, card_2, card_5])
     assert_equal deck.merge_sort, [card_1, card_3, card_4, card_5, card_2]
   end
 
