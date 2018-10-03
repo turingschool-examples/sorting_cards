35aeaa1e945af578f56faa437571c801e07e3b2e
jpclark6 <jpclark6@gmail.com>
2018-10-03 16:22:12 -0600

    Adds round test content

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,10 +7,10 @@ require './lib/round.rb'
 
 class RoundTest < Minitest::Test
   def setup
-    @card_1 = Card.new("Ace", "Spades")
-    card_2 = Card.new("8", "Diamonds")
+    @card_1 = Card.new("3", "Hearts")
+    @card_2 = Card.new("8", "Diamonds")
     card_3 = Card.new("2", "Clubs")
-    @cards = [@card_1, card_2, card_3]
+    @cards = [@card_1, @card_2, card_3]
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
@@ -31,4 +31,43 @@ class RoundTest < Minitest::Test
     assert_equal @card_1, @round.current_card
   end
 
+  def test_return_guess_is_a_guess
+    new_guess = @round.record_guess({value: "3", suit: "Hearts"})
+    assert_instance_of Guess, new_guess
+  end
+
+  def test_it_can_return_if_guess_is_correct
+    new_guess = @round.record_guess({value: "3", suit: "Hearts"})
+    assert new_guess.correct?
+  end
+
+  def test_it_saves_guesses
+    new_guess_1 = @round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 1, @round.guesses.count
+    new_guess_2 = @round.record_guess({value: "5", suit: "Diamonds"})
+    assert_equal new_guess_1, @round.guesses[0]
+    assert_equal new_guess_2, @round.guesses[1]
+    assert_equal 2, @round.guesses.count
+  end
+
+  def test_it_records_correct_guesses_count
+    new_guess_1 = @round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 1, @round.number_correct
+    new_guess_2 = @round.record_guess({value: "5", suit: "Diamonds"})
+    assert_equal 1, @round.number_correct
+    new_guess_3 = @round.record_guess({value: "2", suit: "Clubs"})
+    assert_equal 2, @round.number_correct
+  end
+
+  def test_it_moves_to_next_card_after_guess
+    new_guess_1 = @round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal @card_2, @round.current_card
+  end
+
+  def test_it_can_calculate_percentage_correct
+    new_guess_1 = @round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 100, @round.percent_correct
+    new_guess_2 = @round.record_guess({value: "5", suit: "Diamonds"})
+    assert_equal 50, @round.percent_correct
+  end
 end
