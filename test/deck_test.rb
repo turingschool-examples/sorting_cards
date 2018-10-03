c03e922105915fada34cdb1f639b475f44e99abd
Ben Lee <bendelonlee@gmail.com>
2018-10-03 08:47:36 -0600

    Added test for merge_sort

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,4 +16,14 @@ class CardTest < Minitest::Test
     assert_equal deck.sort, [card_1, card_3, card_4, card_5, card_2]
   end
 
+  def test__merge_sort
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Ace", "Spades")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Jack", "Clubs")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    assert_equal deck.merge_sort, [card_1, card_3, card_4, card_5, card_2]
+  end
+
 end
