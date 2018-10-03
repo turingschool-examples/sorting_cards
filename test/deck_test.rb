c707f58824e7c5eaf01fab9c6d487cb7a32dbcdc
Rene Casco <rene.casco@icloud.com>
2018-10-02 21:41:08 -0600

    Initial commit Iteration 2

---
+++ test/deck_test.rb
@@ -0,0 +1,15 @@
+require './lib/deck'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class DeckTest < Minitest::Test
+
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
+  end
+end
