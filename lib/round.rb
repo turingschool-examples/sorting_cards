a32d97859d6222116ed1ec9e84094acd300ba53b
Anten Skrabec <cebarks@gmail.com>
2018-10-03 16:01:49 -0600

    Iteration 2

---
+++ lib/round.rb
@@ -0,0 +1,36 @@
+require './lib/guess'
+
+class Round
+  attr_reader :guesses, :deck, :current_card
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+    @current_card = deck.cards.first
+  end
+
+  def next_card
+    @current_card = deck.cards.shift
+  end
+
+  def record_guess(card_hash)
+    guessCard = Card.new(card_hash[:value], card_hash[:suit]).to_s
+    guess = Guess.new(guessCard, current_card)
+    guesses << guess
+    next_card
+    guess
+  end
+
+  def number_correct
+    correct = 0
+    guesses.each do |guess|
+      correct += 1 if guess.correct?
+    end
+    correct
+  end
+
+  def percent_correct
+    correct = number_correct
+    p correct, @guesses.count
+    (correct.to_f / @guesses.count.to_f) * 100
+  end
+end
