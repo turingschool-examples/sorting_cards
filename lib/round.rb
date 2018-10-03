fb7b7aace9ba863fb0ac2609f06f31f04ec66cc7
jpclark6 <jpclark6@gmail.com>
2018-10-03 16:06:09 -0600

    Includes round class content

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,33 @@
+require './lib/guess.rb'
+
 class Round
+  attr_reader :deck, :guesses, :number_correct
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+    @card_number = 0
+    @number_correct = 0
+  end
+
+  def current_card
+    @deck.cards[@card_number]
+  end
+
+  def record_guess(new_guess)
+    card = self.current_card
+    guess = Guess.new("#{new_guess[:value]} of #{new_guess[:suit]}", card)
+
+    @guesses.push(guess)
+
+    if guess.correct? then @number_correct += 1 end
+
+    @card_number += 1
+    guess
+  end
+
+  def percent_correct
+    @number_correct.to_f / @guesses.count.to_f * 100.0
+  end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -6,5 +6,29 @@ require './lib/deck.rb'
 require './lib/round.rb'
 
 class RoundTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("Ace", "Spades")
+    card_2 = Card.new("8", "Diamonds")
+    card_3 = Card.new("2", "Clubs")
+    @cards = [@card_1, card_2, card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+  def test_it_exists
+    assert_instance_of Round, @round
+  end
+
+  def test_it_returns_deck
+    assert_equal @deck, @round.deck
+  end
+
+  def test_it_creates_guesses_when_initialized
+    assert_empty @round.guesses
+  end
+
+  def test_it_returns_1st_card_after_initialize
+    assert_equal @card_1, @round.current_card
+  end
 
 end
