84a8df446af1c6165f995741b0bfa10e26d50f83
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-04 15:30:43 -0600

    updated spacing in the round_test file

--- test/round_test.rb
+++ test/round_test.rb
@@ -9,41 +9,32 @@ class RoundTest < Minitest::Test
   def test_does_it_exist
     card_1 = Card.new("3", "Hearts")
     card_2 = Card.new("4", "Clubs")
-
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-
     assert_instance_of Round, round
   end
 
   def test_it_has_a_deck
     card_1 = Card.new("3", "Hearts")
     card_2 = Card.new("4", "Clubs")
-
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-
     assert_equal deck, round.deck
   end
 
   def test_guesses_starts_with_empty_array
     card_1 = Card.new("3", "Hearts")
     card_2 = Card.new("4", "Clubs")
-
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-
     assert_equal [], round.guesses
   end
 
   def test_if_it_has_current_card
     card_1 = Card.new("3", "Hearts")
     card_2 = Card.new("4", "Clubs")
-
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-
-
     assert_equal card_1, round.current_card
   end
 
