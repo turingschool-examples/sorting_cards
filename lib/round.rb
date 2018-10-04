77b50dd2b7d9215c8b76e927f40443b1fcfd803c
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-03 20:30:18 -0600

    Update Round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,27 +1,24 @@
+require './lib/guess'
+
 class Round
 
-  attr_reader :deck
+  attr_reader :deck, :guesses, :current_card
 
   def initialize(deck)
     @deck  = deck
     @guesses = []
-    @current_card = deck.first
+    @current_card = deck.cards.first
     @number_correct = 0
     @percent_correct = 0.0
   end
 
   def record_guess(guess)
-    # creates a Guess object with the guess as the Guess response and the
-    # current_card as the Guess card
     response = "#{guess[:value]} of #{guess[:suit]}"
 # check if can/should add current_card to attr_reader and use getter method
 # instead of referencing instance variable directly
     current_guess = Guess.new(response, @current_card)
-
-    @current_card = deck.rotate(1).first
-
+    @current_card = deck.cards.rotate(1).first
     return current_guess
-
   end
 
 
