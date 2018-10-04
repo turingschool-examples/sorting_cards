0a1c7da561448bf46b1b2c0ae27e1b596f292a8b
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-03 21:00:49 -0600

    Update Round class

--- lib/round.rb
+++ lib/round.rb
@@ -2,22 +2,39 @@ require './lib/guess'
 
 class Round
 
-  attr_reader :deck, :guesses, :current_card
+  attr_reader :deck,
+              :guesses,
+              :current_card,
+              :number_correct,
+              :percent_correct
 
   def initialize(deck)
     @deck  = deck
     @guesses = []
-    @current_card = deck.cards.first
+    @deck_tracker = 0
+    @current_card = deck.cards[@deck_tracker]
     @number_correct = 0
     @percent_correct = 0.0
   end
 
   def record_guess(guess)
     response = "#{guess[:value]} of #{guess[:suit]}"
-# check if can/should add current_card to attr_reader and use getter method
-# instead of referencing instance variable directly
+# check if it's better to use getter methods or access instance
+# variables directly???
     current_guess = Guess.new(response, @current_card)
-    @current_card = deck.cards.rotate(1).first
+
+# Separate this into a separate method?
+    @deck_tracker += 1
+    @current_card = deck.cards[@deck_tracker]
+
+    @guesses << current_guess
+
+# Separate this into a different method?
+    if current_guess.correct?
+      @number_correct += 1
+    end
+    @percent_correct = @number_correct.to_f / @guesses.size.to_f * 100.0
+
     return current_guess
   end
 
