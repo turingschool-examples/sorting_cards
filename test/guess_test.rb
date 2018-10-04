7c374865dadfec42942d83b6099253dd28324b17
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-03 21:17:52 -0600

    Adds guess_test

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,20 +1,36 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/guess'
+require './lib/card'
 
 class GuessTest < Minitest::Test
+
+  def setup
+    @card = Card.new("10", "Hearts")
+  end
+
   def test_it_exists
-    guess = Guess.new()
+    guess = Guess.new("10 of Hearts", @card)
     assert_instance_of Guess, guess
   end
 
-  def test_it_has_a_value
-    card = Card.new("Ace", "Spades")
-    assert_equal "Ace", card.value
+  def test_it_has_a_response
+    guess = Guess.new("10 of Hearts", @card)
+    assert_equal "10 of Hearts", guess.response
   end
 
-  def test_it_has_a_suit
-    card = Card.new("Ace", "Spades")
-    assert_equal "Spades", card.suit
+  def test_it_has_a_card
+    guess = Guess.new("10 of Hearts", @card)
+    assert_equal @card, guess.card
+  end
+
+  def test_it_has_correct
+    guess = Guess.new("10 of Hearts", @card)
+    assert guess.correct?
+  end
+
+  def test_it_gives_feedback
+    guess = Guess.new("10 of Hearts", @card)
+    assert_equal "Correct!" guess.feedback
   end
 end
