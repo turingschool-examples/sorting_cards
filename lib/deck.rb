3db8e9587bf164e559d225f7fd209002a1e98370
Rene Casco <rene.casco@icloud.com>
2018-10-04 09:06:54 -0600

    Iteration 2 completed, started iterarion 3

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,5 @@
 require './lib/card'
+require 'pry'
 
 class Deck
   attr_reader :cards
@@ -9,6 +10,15 @@ class Deck
 
   def count
     cards.count
+  end
+
+  def sort
+      binding.pry
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    binding.pry
+    # iterate through deck
+    #   get current card value find it on a
   end
 
 end
--- lib/round.rb
+++ lib/round.rb
@@ -1,13 +1,16 @@
 require './lib/deck'
 require './lib/guess'
+require 'pry'
 
 class Round
   attr_reader :deck,
-              :guesses
+              :guesses,
+              :number_correct
 
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @number_correct = 0
   end
 
   def current_card
@@ -17,8 +20,13 @@ class Round
   def record_guess(guess)
     response = "#{guess[:value]} of #{guess[:suit]}"
     @guesses << Guess.new(response, current_card)
+    @number_correct += 1 if @guesses.last.correct?
     @deck.cards.shift
     return @guesses.last
+  end
+
+  def percent_correct
+    ((@number_correct.to_f / @guesses.count) * 100).round(1)
   end
 
 
