f6a60349d85264ac99beef1d3e121dc1a896a748
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-04 09:09:56 -0600

    added comparison and standardization method to the Card class, no test yet

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,26 @@
 class Deck
-  attr_reader :cards
+  attr_accessor :cards
   def initialize(*cards)
     @cards = cards
   end
 
   def count
     cards.count
+  end
+
+  def sort
+    sorted = false
+    until sorted
+      cards_duplicate = cards.dup
+      n = cards.length
+      (n-1).times do |i|
+        if cards[i+1] < cards[i]
+          cards[]
+      end
+    end
+  end
+
+  def reverse_sort
+
   end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -5,6 +5,9 @@ require './lib/card'
 class CardTest < Minitest::Test
   def setup
     @card = Card.new("Ace", "Spades")
+    @card_2 = Card.new("Ace", "Clubs")
+    @card_3 = Card.new("Jack", "Hearts")
+    @card_4 = Card.new("3", "Clubs")
   end
 
   def test_it_exists
@@ -17,5 +20,21 @@ class CardTest < Minitest::Test
 
   def test_it_has_a_suit
     assert_equal "Spades", @card.suit
+  end
+
+  def test_it_compares_by_value_less_than
+
+  end
+
+  def test_it_compares_by_value_greater_than
+
+  end
+
+  def test_it_compares_by_suit_less_than
+
+  end
+
+  def test_compares_by_suit_greater_than
+
   end
 end
