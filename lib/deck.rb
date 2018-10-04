91ade1b54ffb975a09bfb8b88a950b493ff7ea97
Tony Robertson <tonyr729@gmail.com>
2018-10-04 08:16:51 -0600

    Add merge sort method to Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -14,13 +14,42 @@ class Deck
   end
 
   def sort
-    (@cards.count - 1).times do |x|
-      (@cards.count - 1 - x).times do |i|
-        if @cards[i].card_weight > @cards[i+1].card_weight
-          @cards[i], @cards[i+1] = @cards[i+1], @cards[i]
+    cards_copy = @cards.dup
+    (cards_copy.count - 1).times do |x|
+      (cards_copy.count - 1 - x).times do |i|
+        if cards_copy[i].card_weight > cards_copy[i+1].card_weight
+          cards_copy[i], cards_copy[i+1] = cards_copy[i+1], cards_copy[i]
         end
       end
     end
-    @cards
+    cards_copy
+  end
+
+  def merge_sort
+    cards_copy = @cards.dup
+    seperate(cards_copy)
+  end
+
+  def seperate(cards)
+    if cards.count <= 1
+      cards
+    else
+      mid = (cards.count / 2)
+      left = seperate(cards[0..mid - 1])
+      right = seperate(cards[mid..cards.count])
+      merge(left, right)
+    end
+  end
+
+  def merge(left, right)
+    if left.empty?
+      right
+    elsif right.empty?
+      left
+    elsif left[0].card_weight < right[0].card_weight
+      [left[0]] + merge(left[1..left.count], right)
+    else
+      [right[0]] + merge(left, right[1..right.count])
+    end
   end
 end
