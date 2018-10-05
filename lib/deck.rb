e857d6b42dc4055502d0e7ec3b9795833e3561a7
Rene Casco <rene.casco@icloud.com>
2018-10-04 21:36:43 -0600

    Iteration 3 completed, refactoring pending

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,7 @@
 require './lib/card'
 require 'pry'
 
+
 class Deck
   attr_reader :cards
 
@@ -13,12 +14,21 @@ class Deck
   end
 
   def sort
-      binding.pry
     values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    binding.pry
-    # iterate through deck
-    #   get current card value find it on a
+    return @cards if @cards.length <= 1
+    swapped = true
+    while swapped
+      swapped = false
+      i = 0
+      while i < @cards.length - 1
+        if (@cards[i].value == @cards[i+1].value && @cards[i].suit > @cards[i+1].suit) || values.find_index(@cards[i].value) > values.find_index(@cards[i+1].value)
+          @cards[i], @cards[i+1] = @cards[i+1], @cards[i]
+          swapped = true
+        end
+        i += 1
+      end
+    end
+    return @cards
   end
-
+  
 end
