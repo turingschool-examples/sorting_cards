94bc250d9a6b8be72099cf58d0c3b3a1eb4d9f17
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-04 22:05:02 -0600

    Finish Iteration 2 of project with passing test file.

--- lib/guess.rb
+++ lib/guess.rb
@@ -14,9 +14,9 @@ class Guess
 
   def feedback
     if correct? == true
-      puts "Correct!"
+      "Correct!"
     else
-      puts "Incorrect."
+      "Incorrect."
     end
   end
 
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -2,3 +2,72 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/guess'
+
+class GuessTest < Minitest::Test
+
+  def test_the_guess_exists
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_instance_of Guess, guess
+  end
+
+  def test_the_guess_has_a_card
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal card, guess.card
+  end
+
+  def test_the_guess_has_a_response
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal "10 of Hearts", guess.response
+  end
+
+  def test_the_guess_evalutes_correctness
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal true, guess.correct?
+  end
+
+  def test_the_guess_returns_feedback
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_equal "Correct!" , guess.feedback
+  end
+end
+
+
+# def setup
+#   @card = Card.new("10", "Hearts")
+#   @guess = Guess.new("10 of Hearts", @card)
+# end
+#
+# def test_the_guess_exists
+#   assert_instance_of Guess, @guess
+# end
+#
+# def test_the_guess_has_a_card
+#   assert_equal @card, @guess.@card
+# end
+#
+# def test_the_guess_has_a_response
+#   skip
+#   card = Card.new("10", "Hearts")
+#   guess = Guess.new("10 of Hearts", card)
+#   assert_equal "10 of Hearts", guess.response
+# end
+#
+# def test_the_guess_evalutes_correctness
+#   skip
+#   card = Card.new("10", "Hearts")
+#   guess = Guess.new("10 of Hearts", card)
+#   assert_equal true, guess.correct?
+# end
+#
+# def test_the_guess_returns_feedback
+#   skip
+#   card = Card.new("10", "Hearts")
+#   guess = Guess.new("10 of Hearts", card)
+#   assert_equal "Correct!" , guess.feedback
+# end
+# end
