07b0860d49024b9d6fbeef3ab2fb6c61f21e960d
Daniel Briechle <danbriechle@hotmail.com>
2018-10-06 16:40:12 -0600

    Removed correct? method added interpolation to record_guess method

--- lib/round.rb
+++ lib/round.rb
@@ -4,23 +4,21 @@ require './lib/card'
 
 
 class Round
-    attr_reader :deck, :current_card
+    attr_reader :deck, :current_card, :current_guess
     attr_accessor :guesses
 
      def initialize(deck)
        @deck = deck
        @guesses = []
        @current_card = deck.cards.shift
+       @current_guess = current_guess
      end
 
      def record_guess(new_guess)
-       current_guess = Guess.new(@current_card, new_guess)
+       current_guess = Guess.new("#{new_guess[:value]} of #{new_guess[:suit]}", @current_card)
        @guesses << current_guess
        return current_guess
      end
 
-     def correct?
-       @current_card == guesses.cards[0]
-     end
 
 end
