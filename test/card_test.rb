7bc5c2db78f058d631b8c62aeeed58072f8b02d2
Daniel Briechle <danbriechle@hotmail.com>
2018-10-06 10:50:01 -0600

    Removes pry and comments

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require 'pry'
 
 class CardTest < Minitest::Test
   def test_it_exists
@@ -9,16 +8,12 @@ class CardTest < Minitest::Test
     assert_instance_of Card, card
   end
 
-  def test_it_has_a_value 
-    # skip
-
+  def test_it_has_a_value
     card = Card.new("Ace", "Spades")
     assert_equal "Ace", card.value
   end
-binding.pry
-  def test_it_has_a_suit
-    # skip
 
+  def test_it_has_a_suit
     card = Card.new("Ace", "Spades")
     assert_equal "Spades", card.suit
   end
