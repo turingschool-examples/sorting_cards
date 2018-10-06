75681ccbc085aee27f1187c658f9bb95d5ecb7c7
Daniel Briechle <danbriechle@hotmail.com>
2018-10-06 11:26:28 -0600

    fixes indentation in guess test

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -13,59 +13,55 @@ class GuessTest < Minitest::Test
     assert_instance_of Guess, guess
   end
 
-def test_it_has_a_card
-  card = Card.new("10", "Hearts")
-  guess = Guess.new("10 of Hearts", card)
+  def test_it_has_a_card
+     card = Card.new("10", "Hearts")
+     guess = Guess.new("10 of Hearts", card)
 
-   assert_instance_of Card, guess.card
-   assert_equal card, guess.card
-end
-
-
-def test_it_has_a_response
-  card = Card.new("10", "Hearts")
-  guess = Guess.new("10 of Hearts", card)
-
-  assert guess.response
-end
-
-def test_correct_verifies_guess
-  card = Card.new("10", "Hearts")
-  guess = Guess.new("10 of Hearts", card)
-
-  assert guess.correct?
-end
+     assert_instance_of Card, guess.card
+     assert_equal card, guess.card
+  end
 
 
-def test_it_gives_feedback
-  card = Card.new("10", "Hearts")
-  guess = Guess.new("10 of Hearts", card)
+  def test_it_has_a_response
+     card = Card.new("10", "Hearts")
+     guess = Guess.new("10 of Hearts", card)
 
-  guess.correct?
+     assert guess.response
+  end
 
-  assert_equal "True!", guess.feedback
-end
-# guess.feedback
+  def test_correct_verifies_guess
+     card = Card.new("10", "Hearts")
+     guess = Guess.new("10 of Hearts", card)
+
+     assert guess.correct?
+  end
 
 
+  def test_it_gives_feedback
+     card = Card.new("10", "Hearts")
+     guess = Guess.new("10 of Hearts", card)
 
-#
+     guess.correct?
+
+     assert_equal "True!", guess.feedback
+  end
+
 # card = Card.new("Queen", "Clubs")
 # #=> #<Card:0x00007f99839aa2a8 @suit="Clubs", @value="Queen">
-
+#
 # guess = Guess.new("2 of Diamonds", card)
 # #=> #<Guess:0x00007f998413ee60 @card=#<Card:0x00007f99839aa2a8 @suit="Clubs", @value="Queen">, @guess="2 of Diamonds">
-
+#
 # guess.card
 #   #<Card:0x00007f99839aa2a8 @suit="Clubs", @value="Queen">
-
+#
 # guess.response
 #  # "2 of Diamonds"
-
+#
 # guess.correct?
 #  # false
-
+#
 # guess.feedback
-#  # "Incorrect."# 
+#  # "Incorrect."#
 
 end
