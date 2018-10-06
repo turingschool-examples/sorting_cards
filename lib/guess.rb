5993f6894f14077c53c58363c187b993ce047ff0
Daniel Briechle <danbriechle@hotmail.com>
2018-10-06 11:13:50 -0600

    Creates guess class with correct? and feedback methods

---
+++ lib/guess.rb
@@ -0,0 +1,22 @@
+
+class Guess
+  attr_reader :response, :card
+  def initialize (response, card)
+    @response = response
+    @card = card
+
+  end
+  def correct?
+
+    [@card.value, @card.suit] == @response.split(' of ')
+  end
+
+  def feedback
+    if correct? == true
+      p "True!"
+    else
+      p "Wrong!"
+    end
+  end
+
+end
