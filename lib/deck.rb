cb5a3e093972dbd9f0c2789aa639ef52e2a27c9b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-07 12:15:12 -0600

    Delete: sorting_test.rb. Test: Move to deck_test for sort method

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,3 +11,14 @@ class Deck
     cards.length
   end
 end
+
+# compare a pair of adjacent values
+# if
+# lowest to highest is 2 through 10, Jack, Queen, King, Ace.
+
+#repeat
+
+#compare adjacent suits
+#lowest to highest is Clubs, Diamonds, Hearts, Spades.
+
+#repeat
