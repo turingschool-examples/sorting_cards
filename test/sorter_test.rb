18b7df3e1ab5159f424c0fb629c28f75a362a073
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-07 16:53:12 -0600

    Add ScoreCards module to sorter class

--- test/sorter_test.rb
+++ test/sorter_test.rb
@@ -4,7 +4,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
-require './lib/round'
 require './lib/sorter.rb'
 
 class SorterTest < Minitest::Test
@@ -14,6 +13,7 @@ class SorterTest < Minitest::Test
     @card_3 = Card.new("5", "Diamonds")
     @card_4 = Card.new("Jack", "Clubs")
     @card_5 = Card.new("Ace", "Diamonds")
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5]
     @deck = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5])
     @sorter = Sorter.new([@card_1, @card_2, @card_3, @card_4, @card_5])
   end
@@ -22,10 +22,11 @@ class SorterTest < Minitest::Test
     assert_instance_of Sorter, @sorter
   end
 
-  def test_it_values_deck
-    values = {@card_1 => [2, 2], @card_2 => [12, 3], @card_3 => [3, 1],
-              @card_4 => [9, 0], @card_5 => [12, 1]}
-    assert_equal values, @sorter.score_deck
+  def test_it_scores_cards
+    scored_cards = [[@card_1, 2.2], [@card_2, 12.3],
+                    [@card_3, 3.1], [@card_4, 9.0], [@card_5, 12.1]]
+
+    assert_equal scored_cards, @sorter.score_cards(@cards)
   end
 
   def test_it_sorts
