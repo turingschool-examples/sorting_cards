abbc0627e386e6478465a22ec494b764ee2f4d1e
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-07 13:52:08 -0600

    Adds merge sorter

--- lib/merge_sorter.rb
+++ lib/merge_sorter.rb
@@ -1,3 +1,45 @@
+require 'pry'
+
 class MergeSorter
+
+  VALUES_SORT_ORDER = {"2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4,
+                      "7" => 5, "8" => 6, "9" => 7, "10" => 8, "Jack" => 9,
+                      "Queen" => 10, "King" => 11, "Ace" => 12}
+  SUITS_SORT_ORDER = {"Clubs" => 0, "Diamonds" => 1,
+                      "Hearts" => 2, "Spades" => 3}
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def score_cards
+    @cards.map do |card|
+      value_score = VALUES_SORT_ORDER[card.value]
+      suit_score = SUITS_SORT_ORDER[card.suit]
+      [card, "#{value_score}.#{suit_score}"]
+    end
+  end
+
+  def merge_sort
+  end
+
+  def merge(left_list, right_list)
+    sorted_list = []
+
+    total_length = left_list.size + right_list.size
+
+    total_length.times do
+
+      if left_list[0][1] < right_list[0][1]
+        sorted_list << left_list.shift
+      else
+        sorted_list << right_list.shift
+      end
+      if left_list.length == 0
+        sorted_list << right_list
+    end
+    sorted_list
+  end
+end
 
 end
--- test/merge_sorter_test.rb
+++ test/merge_sorter_test.rb
@@ -1,25 +1,48 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require './lib/deck'
 require './lib/merge_sorter'
 
 class MergeSorterTest < Minitest::Test
 
-def setup
-  @card_1 = Card.new("4","Hearts")
-  @card_2 = Card.new("Ace", "Spades")
-  @card_3 = Card.new("5", "Diamonds")
-  @card_4 = Card.new("Jack", "Clubs")
-  @card_5 = Card.new("Ace", "Diamonds")
-  @deck = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5])
-end
+  def setup
+    @card_1 = Card.new("4","Hearts")
+    @card_2 = Card.new("Ace", "Spades")
+    @card_3 = Card.new("5", "Diamonds")
+    @card_4 = Card.new("Jack", "Clubs")
+    @card_5 = Card.new("Ace", "Diamonds")
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5]
+    @merge_sorter = MergeSorter.new(@cards)
+  end
 
-def test_it_exists
-  assert_instance_of MergeSorter, MergeSorter.new
-end
+  def test_it_exists
+    assert_instance_of MergeSorter, @merge_sorter
+  end
 
+  def test_it_scores_cards
+    scored_cards = [[@card_1, "2.2"], [@card_2, "12.3"],
+                    [@card_3, "3.1"], [@card_4, "9.0"], [@card_5, "12.1"]]
 
+    assert_equal scored_cards, @merge_sorter.score_cards
+  end
 
+  def test_it_merge_sorts_cards
+    skip
+    expected = [@card_1, @card_3, @card_4, @card_5, @card_2]
+    assert_equal expected, @merge_sorter.merge_sort
+  end
+
+  def test_it_merges_cards
+    merge_sorter = MergeSorter.new([@card_1, @card_3])
+    actual = @merge_sorter.merge([[@card_1, "2.2"]], [[@card_3, "3.1"]])
+    assert_equal [[@card_1, "2.2"], [@card_3, "3.1"]], actual
+  end
+
+  def test_it_sorts # use deck for this?
+    skip
+    sorted_cards = [@card_1, @card_3, @card_4, @card_5, @card_2]
+
+    assert_equal sorted_cards, @merge_sorter.merge_sort
+  end
 
 end
