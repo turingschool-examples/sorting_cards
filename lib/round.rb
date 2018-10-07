3dd5c597fe3358f26cd48e1527fe128826260f6c
Daniel Briechle <danbriechle@hotmail.com>
2018-10-06 18:57:06 -0600

    lost mind trying to count guesses

--- lib/round.rb
+++ lib/round.rb
@@ -4,14 +4,16 @@ require './lib/card'
 
 
 class Round
-    attr_reader :deck, :current_card, :current_guess
-    attr_accessor :guesses
+    attr_reader :deck, :current_card, :current_guess,:guesses, :correct, :incorrect
 
      def initialize(deck)
        @deck = deck
        @guesses = []
        @current_card = deck.cards.shift
        @current_guess = current_guess
+       @correct = 0
+       @incorrect = 0
+
      end
 
      def record_guess(new_guess)
@@ -20,5 +22,9 @@ class Round
        return current_guess
      end
 
-
+     if Guess.new(new_guess).correct? == true
+       @correct += 1
+     else
+       @incorrect += 1
+    end
 end
