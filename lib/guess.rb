5e683aa83f5bb53edf48bd107a2efeaa60cddd62
Ben Lee <bendelonlee@gmail.com>
2018-10-07 07:06:16 -0600

    Refactoring

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,16 +1,16 @@
 require_relative 'card'
 class Guess
-  attr_reader :card, :guess, :guessed_card
+  attr_reader :card, :guess, :g_card
 
   def initialize(string, card)
     @guess = string
     @card = card
     arr = response.split
-    @guessed_card = Card.new(arr[0], arr[-1])
+    @g_card = Card.new(arr[0], arr[-1])
   end
 
   def correct?
-     card.value == guessed_card.value && card.suit == guessed_card.suit
+     card.value == g_card.value && card.suit == g_card.suit
   end
 
   def feedback
