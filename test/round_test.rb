6a58fbbf320297db419f7af8aa2a56fc24dc9de5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-07 08:48:53 -0600

    Tests: give_feedback_for_the_last_guess

--- test/round_test.rb
+++ test/round_test.rb
@@ -67,6 +67,12 @@ class RoundTest < Minitest::Test
     assert_equal 2, @round.guesses.count
   end
 
+  def test_it_can_give_feedback_for_the_last_guess
+    new_guess = @round.record_guess({value: "3", suit: "Hearts"})
+    new_guess = @round.record_guess({value: "Jack", suit: "Diamonds"})
+    assert_equal "Incorrect.", @round.guesses.last.feedback
+  end
+
   def test_it_can_record_the_percent_correct
     new_guess = @round.record_guess({value: "3", suit: "Hearts"})
     new_guess = @round.record_guess({value: "Jack", suit: "Diamonds"})
