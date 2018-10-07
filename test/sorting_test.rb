eb8a7b0b8695674be85a695d3d673c38511065d4
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-07 09:12:28 -0600

    Tests: def setup is complete

--- test/sorting_test.rb
+++ test/sorting_test.rb
@@ -9,10 +9,10 @@ require './lib/sorting'
 
 class SortingTest < Minitest::Test
   def setup
-    card_1 = Card.new("4","Hearts")
-    card_2 = Card.new("Ace", "Spades")
-    card_3 = Card.new("5", "Diamonds")
-    card_4 = Card.new("Jack", "Clubs")
-    card_5 = Card.new("Ace", "Diamonds")
-    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    @card_1 = Card.new("4","Hearts")
+    @card_2 = Card.new("Ace", "Spades")
+    @card_3 = Card.new("5", "Diamonds")
+    @card_4 = Card.new("Jack", "Clubs")
+    @card_5 = Card.new("Ace", "Diamonds")
+    @deck = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5])
   end
