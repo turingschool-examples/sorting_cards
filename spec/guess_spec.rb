6a9cb69dbca6f28867055935c1a259b92b98dd42
Jorja Fleming <aset284@gmail.com>
2023-04-20 11:23:41 -0600

    Add card code

--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -3,31 +3,43 @@ require './lib/guess'
 require 'rspec'
 
 RSpec.describe Guess do
-  let(:ten_of_hearts) {Card.new("10", "Hearts")}
-
+  
   it "exists and has attributes" do
-    guess = Guess.new("10 of Hearts", ten_of_hearts)
+    card = Card.new("Queen", "Clubs")
+    guess = Guess.new("2 of Diamonds", card)
     expect(guess).to be_instance_of(Guess)
-    expect(ten_of_hearts).to be_instance_of(Card)
+    expect(card).to be_instance_of(Card)
   end
 
   xit 'can make a guess' do 
-    guess = Guess.new("10 of Hearts", ten_of_hearts)
-    expect(guess.card).to eq(ten_of_hearts)
+    card = Card.new("Queen", "Clubs")
+    guess = Guess.new("2 of Diamonds", card)
+    expect(guess.card).to eq(card)
+  end
+
+  xit "can get a response" do
+    card = Card.new("Queen", "Clubs")
+    guess = Guess.new("2 of Diamonds", card)
+    expect(guess.response).to eq("2 of Diamonds")
   end
   
-  xit "can give feedback for a guess" do
+  xit "can be correct or incorrect" do
     card = Card.new("Queen", "Clubs")
-    guess = Guess.new("Queen of Clubs", card)
-    expect(guess.card).to eq(card)
-    expect(guess.response).to eq("Queen of Clubs")
+    guess = Guess.new("2 of Diamonds", card)
+    expect(guess.correct?).to eq(false)
+    
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
     expect(guess.correct?).to eq(true)
+  end
+
+  xit "can give feedback" do 
+    card = Card.new("Queen", "Clubs")
+    guess = Guess.new("2 of Diamonds", card)
+    expect(guess.feedback).to eq("Incorrect.")
+
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
     expect(guess.feedback).to eq("Correct!")
-        
-    guess2 = Guess.new("Ace of Spades", card)
-    expect(guess2.card).to eq(card)
-    expect(guess2.response).to eq("Ace of Spades")
-    expect(guess2.correct?).to eq(false)
-    expect(guess2.feedback).to eq("Incorrect.")
   end
 end
