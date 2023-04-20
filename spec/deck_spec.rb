e8e2cd56a74b7ed8d41c1a47fd8af61346f26687
Jorja Fleming <aset284@gmail.com>
2023-04-20 14:06:33 -0600

    Add deck code

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe Deck do
   end
 
   it "contains cards" do
-    deck = Deck.new
-    #lets try shovling these into an array
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
     card_3 = Card.new("5", "Diamonds")
-    expect(deck).to eq([card_1, card_2, card_3])
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq([card_1, card_2, card_3])
     expect(deck.count).to eq(3)
   end
 end
