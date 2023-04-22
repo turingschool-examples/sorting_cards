7be350cc3f5eeec347a45ce1b51b3ae829dce8c4
Jorja Fleming <aset284@gmail.com>
2023-04-22 17:06:57 -0600

    Add explanations of both methods I used to solve

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,14 +9,20 @@ class Deck
     @cards.size
   end
 
-  # def sort
-  #   sorted = []
-  #   while @cards.size > 0
-  #     smallest = @cards.min_by { |x| x.point_value}
-  #     sorted << @cards.delete(smallest)
-  #   end
-  #   @cards = sorted
-  # end
+  def sort
+    sorted = []
+    while @cards.size > 0
+      smallest = @cards.min_by { |x| x.point_value}
+      sorted << @cards.delete(smallest)
+    end
+    @cards = sorted
+  end
+
+  # #  i first created a way to assign each card a numeric value, then 
+  # i used a min_by method to iterate through the array of cards and 
+  # then pull out the smallest card and put it into a new array while 
+  # deleting that card from the old array. Once it has completed iteration 
+  # thru the original array it returns the new sorted array. 
 
   def sort
     n = @cards.length
@@ -34,4 +40,12 @@ class Deck
     end
     @cards
   end
+
+# But then I wanted to try the bubble sort and for that one you start by 
+# figuring out the array length then you create a loop that goes thru and 
+# compares the first two numbers if the first number is smaller it's place
+# does not change but if the second card is smaller than the first the 
+# two cards will be swapped. As each card gets moved swapped becomes true 
+# and you tell the loop to stop once there has been no more swapping 
+
 end
